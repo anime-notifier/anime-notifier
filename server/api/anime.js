d6b66a9165e95c1ef7d7644570b6c0ea914a4b72
Vija02 <salim_michaelmi@yahoo.com>
2017-04-11 20:11:39 +0100

    feat: send data progressively

--- server/api/anime.js
+++ server/api/anime.js
@@ -5,27 +5,30 @@ const popura = require('popura');
 const client = popura(process.env.MAL_USER, process.env.MAL_PASSWORD);
 
 exports.setAnimeList = (data, socket) => {
+  // Get anime list from MAL with username
   client.getAnimeList(data.userName)
   .then(malList => {
+    // Filter watching anime
     let animeWatching = malList.list.filter((a) => {
       return a.my_status === 1;
     });
+    // Send anime list
+    socket.emit('anime', {type: 'malAnimeList', malAnimeList: animeWatching})
+    // Map for ease of use
     animeWatching = animeWatching.map((a) => {
       return {title: a.series_title, episode: a.my_watched_episodes};
     })
-    const promises = [];
+    // Check the availability for each anime
     animeWatching.forEach((a, i) => {
-      promises.push(new Promise((resolve, reject) => {
-        setTimeout(() => {
-          nyaa.checkEpisode(a.title, a.episode + 1).then((available) => {
-            resolve({title: a.title, available})
-          }).catch(err => {reject(err)})
-        }, 500 * i);
-      }))
+      // Set delay to not flood nyaa.se
+      setTimeout(() => {
+        // Check for the next episode from last watched
+        nyaa.checkEpisode(a.title, a.episode + 1).then((available) => {
+          // Send data
+          socket.emit('anime', {type: 'setAnimeList', animeList: {title: a.title, available}})
+        }).catch(err => {console.log(err)})
+      }, 500 * i);
     })
-    Promise.all(promises).then((response) => {
-      socket.emit('anime', {type: 'setAnimeList', animeList: response})
-    });
   })
   .catch(err => console.log(err));
 }

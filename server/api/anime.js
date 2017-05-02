92d1880af2d355aa2528ed3023ab3b2518e163af
Vija02 <salim_michaelmi@yahoo.com>
2017-05-02 21:38:28 +0100

    feat: handle airing episode from database

--- server/api/anime.js
+++ server/api/anime.js
@@ -1,6 +1,9 @@
 "use strict";
-// const nyaa = require('nyaa-available');
 const popura = require('popura');
+const knex = require('../database').knex;
+
+const addDays = require('date-fns/add_days');
+const isFuture = require('date-fns/is_future')
 
 const client = popura(process.env.MAL_USER, process.env.MAL_PASSWORD);
 
@@ -26,16 +29,45 @@ exports.setAnimeList = (data, socket) => {
         socket.emit('anime', {type: 'setAnimeList', animeList: {title: animeWatching[i].title, available: true}});
         continue;
       }
-      // Temporary replacement for nyaa.se
-      socket.emit('anime', {type: 'setAnimeList', animeList: {title: animeWatching[i].title, available: "error"}});
-      // // Set delay to not flood nyaa.se
-      // setTimeout(() => {
-      //   // Check for the next episode from last watched
-      //   nyaa.checkEpisode(a.title, a.episode + 1).then((available) => {
-      //     // Send data
-      //     socket.emit('anime', {type: 'setAnimeList', animeList: {title: a.title, available}})
-      //   }).catch(err => {console.log(err)})
-      // }, 500 * i);
+      // TODO: Send when the anime will air
+      // Else check database
+      knex('animes').select('*').where({name: animeWatching[i].title}).then((model) => {
+        if(model.length !== 0){
+          // If anime schedule is changed
+          if(model[0].next_episode && model[0].next_air){
+            // If the next episode is lower than what is going to be out
+            if(animeWatching[i].episode + 1 < model[0].next_episode){
+              // Then it is available
+              socket.emit('anime', {type: 'setAnimeList', animeList: {title: animeWatching[i].title, available: true}});
+            }else{ // Next episode is higher than that, calculate it
+              const nextEpisode = addDays(new Date(model[0].next_air), 7 * (animeWatching[i].episode + 1 - model[0].next_episode))
+              // If next episode is in the future
+              if(isFuture(nextEpisode)){
+                // Then it is not available
+                socket.emit('anime', {type: 'setAnimeList', animeList: {title: animeWatching[i].title, available: false}});
+              }else{
+                // If in the past, then it is available
+                socket.emit('anime', {type: 'setAnimeList', animeList: {title: animeWatching[i].title, available: true}});
+              }
+            }
+            return;
+          }
+
+          // No schedule change
+          const nextEpisode = addDays(new Date(model[0].air_date), 7 * animeWatching[i].episode);
+          // If next episode is in the future
+          if(isFuture(nextEpisode)){
+            // Then it is not available
+            socket.emit('anime', {type: 'setAnimeList', animeList: {title: animeWatching[i].title, available: false}});
+          }else{
+            // If in the past, then it is available
+            socket.emit('anime', {type: 'setAnimeList', animeList: {title: animeWatching[i].title, available: true}});
+          }
+        }else{
+          // Data not found in the database
+          socket.emit('anime', {type: 'setAnimeList', animeList: {title: animeWatching[i].title, available: "error"}});
+        }
+      })
     }
   })
   .catch(err => console.log(err));

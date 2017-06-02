72a67d6106eb12a1deb07135b2f123135ff5a0f7
Vija02 <salim_michaelmi@yahoo.com>
2017-06-02 15:18:38 +0100

    feat: validate login data

--- server/api/user.js
+++ server/api/user.js
@@ -56,28 +56,41 @@ exports.register = function (data, socket) {
 };
 
 exports.login = function (data, socket) {
-  let userData = null;
-  // Get User Data
-  knex('users').select('*').where({email: data.email}).then((user) => {
-    // If user exist
-    if (user.length > 0) {
-      // Store then user data
-      userData = user[0];
-      // Hash the inputted password
-      return bcryptjs.hash(data.password, user[0].salt);
-    }
-    throw new Error('Invalid username or password');
-  }).then((hashedPassword) => {
-    // If password is correct
-    if (userData.password === hashedPassword) {
-      socket.handshake.session.user_id = userData.id;
-      socket.handshake.session.save();
-      socket.emit('status', {type: 'status', about: 'login', status: "success"});
+  const schema = {
+    email: Joi.string().email().required(),
+    password: Joi.string().min(8).required(),
+  };
+  Joi.validate(data, schema, err => {
+    if(err) {
+      socket.emit('status', {type: 'status', about: 'login', status: "error", error: 'Bad data'});
       return;
     }
-    throw new Error('Invalid username or password');
-  }).catch((error) => {
-    socket.emit('status', {type: 'status', about: 'login', status: "error", error: error.toString()});
+
+    const { email, password } = data;
+
+    let userData = null;
+    // Get User Data
+    knex('users').select('*').where({email: email}).then((user) => {
+      // If user exist
+      if (user.length > 0) {
+        // Store then user data
+        userData = user[0];
+        // Hash the inputted password
+        return bcryptjs.hash(password, user[0].salt);
+      }
+      throw new Error('Invalid username or password');
+    }).then((hashedPassword) => {
+      // If password is correct
+      if (userData.password === hashedPassword) {
+        socket.handshake.session.user_id = userData.id;
+        socket.handshake.session.save();
+        socket.emit('status', {type: 'status', about: 'login', status: "success"});
+        return;
+      }
+      throw new Error('Invalid username or password');
+    }).catch((error) => {
+      socket.emit('status', {type: 'status', about: 'login', status: "error", error: error.toString()});
+    });
   });
 };
 

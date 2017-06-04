ccc5b42044404a3632c8f7adff0bede6a289471e
Vija02 <salim_michaelmi@yahoo.com>
2017-06-04 21:46:39 +0100

    fix: forgot /

--- server/routes.js
+++ server/routes.js
@@ -3,9 +3,9 @@
 // Routes
 module.exports = function (app, controller) {
   // Auth
-  app.post('api/login', controller.handleLogin);
-  app.post('api/register', controller.handleRegister);
-  app.use('api/logout', controller.handleLogout);
+  app.post('/api/login', controller.handleLogin);
+  app.post('/api/register', controller.handleRegister);
+  app.use('/api/logout', controller.handleLogout);
 
   // Everything else
   app.use('*', controller.handleError);
